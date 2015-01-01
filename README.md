02b31302a7735d28ebc05ef0fc4e8485a654c20f
Babken Vardanyan <483ken@gmail.com>
2015-01-01 23:23:21 +0400

    bunch of changes, but doesnt download proper files

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-Python-Programming-Society-Bot
+python-download-all-facebook-group-files
 ==============================
 
-Downloads all files from the Python Programming Society Facebook group
+Downloads all files from specified Facebook group
 
-Requires the FacePy module.
-Install using:
-sudo pip install facepy
+1. Install the FacePy module:
 
-Generate your Facebook access token here:
-https://developers.facebook.com/tools/explorer/
+    $ pip install --user facepy
+
+2. Generate your Facebook access token from here and make sure to tick all the boxes. Some of the boxes might not be necessary, but I am too lazy to find out which ones.
+
+    https://developers.facebook.com/tools/explorer/
+
+3. Run the program and provide the group ID and the access token:
+
+    $ ./dl\_fb\_files.py _GROUPID_ _ACCESSTOKEN_
+
+For example:
+
+    $ ./dl\_fb\_files.py 666666666666666 CaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCCaaCC
+4. The program will create a directory with the name of group id and place all the files from group there
--- dl_fb_files.py
+++ dl_fb_files.py
@@ -1,47 +1,56 @@
 #!/usr/bin/env python
-
-from facepy import GraphAPI
+from __future__ import print_function
 import json
 import requests
 import os
+import os.path
+import errno
+import sys
+import urllib
 
-#install facepy module. Use pip install facepy
-def downloadFile(url, fileName):
-    r = requests.get(url, stream = True)
+import facepy
+
+
+def download_file(url):
+    r = requests.get(url, stream=True)
+    fileNameRaw = url.split('/')[-1]
+    fileName = urllib.parse.unquote(fileNameRaw)
+
+    with open(fileName, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk:
+                f.write(chunk)
+                f.flush()
+
+
+def dl_fb_files(groupId, accessToken):
+    graph = facepy.GraphAPI(accessToken)
+    getString = '/v2.2/' + groupId + '/files'
+    pages = graph.get(getString, page=True, retry=5, limit=2000)
+
+
     try:
-        with open(fileName, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024): 
-                if chunk:
-                    f.write(chunk)
-                    f.flush()
-    except:
-        fileName = fileName.strip('/')
-        with open(fileName, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024): 
-                if chunk:
-                    f.write(chunk)
-                    f.flush()
+        os.makedirs(groupId)
+    except OSError as e:
+        if e.errno == errno.EEXIST and os.path.isdir(groupId):
+            pass
+        else: raise
+
+    os.chdir(os.path.join(os.getcwd(), groupId))
 
 
+    for page in pages:
+        for post in page['data']:
+            url = post['download_link']
+            print('fetching ' + url)
+            download_file(url)
 
-groupId = "714685238558448"
-#Generate access token here
-#https://developers.facebook.com/tools/explorer/
-accessToken = "###"
 
-graph = GraphAPI(accessToken)
-pages = graph.get(groupId + "/files", page = True, retry = 5, limit = 2000)
-counter = 0
+if __name__ == '__main__':
+    if not len(sys.argv) > 2:
+        print('Usage:')
+        print('$ ./dl_fb_files.py GROUPID ACCESSTOKEN')
+        sys.exit(1)
 
-os.mkdir('fbPythonGroup')
-os.chdir(os.getcwd() + '/fbPythonGroup')
-
-for page in pages:
-    for post in page['data']:
-        url = post['download_link']
-        #fileName is extracted from download url of the following type
-        #https://www.facebook.com/download/837214882985735/scipy-ref-0.14.0.pdf
-        fileName = url[50:]
-        print "fetching " + url
-        downloadFile(url, fileName)
+    dl_fb_files(sys.argv[1], sys.argv[2])
 
